ad303575516fc15ec19ba1a36bfdc22d7a03943e
meethereum <meethbackup@gmail.com>
2025-03-19 21:58:48 +0530

    added first transform func for CAS

--- store_test.go
+++ store_test.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"testing"
 )
 
+func TestPathTransformFunc(t *testing.T){
+	key:="mybestpicture"
+	pathname:=CASPathTransformFunc(key)
+	fmt.Println(pathname)
+	expectedPathName := "be17b/32c28/70b1c/0c73b/59949/db6a3/be781/4dd23"
+	if pathname!=expectedPathName{
+		t.Errorf("have %s and want %s",pathname,expectedPathName)
+	}
+}
+
 func TestStore(t *testing.T){
 	opts:=StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 	s:=NewStore(opts)
 
